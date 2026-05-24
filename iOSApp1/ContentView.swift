057583898257dd166b1583c72cae57dae28d93a1
Stephanie Otteson <vibrantimpact@myyahoo.com>
2026-05-24 13:55:09 -0600

    Initial Commit

---
+++ iOSApp1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSApp1
+//
+//  Created by stephanie otteson on 2026-05-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSApp1/iOSApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp1App.swift
+//  iOSApp1
+//
+//  Created by stephanie otteson on 2026-05-24.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
