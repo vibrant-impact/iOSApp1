9b970c6b59e596f10ed577db05ddeb1dd2036a97
Stephanie Otteson <vibrantimpact@myyahoo.com>
2026-05-28 13:38:02 -0600

    Separated WelcomeView and ManifestBuilderView from ContentView

---
+++ iOSApp1/Views/ManifestBuilderView.swift
@@ -0,0 +1,18 @@
+//
+//  ManifestBuilderView.swift
+//  iOSApp1
+//
+//  Created by stephanie otteson on 2026-05-28.
+//
+
+import SwiftUI
+
+struct ManifestBuilderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManifestBuilderView()
+}
---
+++ iOSApp1/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  iOSApp1
+//
+//  Created by stephanie otteson on 2026-05-28.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
